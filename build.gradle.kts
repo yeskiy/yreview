1c8a405db708c08630813ec068342085920c0d44
yeskiy <39830788+yeskiy@users.noreply.github.com>
2026-08-20 08:45:52 +0300

    chore: scaffold the idea review plugin build

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+    kotlin("plugin.serialization") version "2.4.10"
+    id("org.jetbrains.intellij.platform")
+}
+
+group = "com.yeskiy"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2026.2.1")
+        bundledPlugin("Git4Idea")
+    }
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "com.yeskiy.ideareview"
+        name = "IDEA Review"
+        version = project.version.toString()
+        ideaVersion {
+            sinceBuild = "262"
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.jetbrains.intellij.platform.settings") version "2.18.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "idea-review"
